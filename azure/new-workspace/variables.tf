7b43779578b5c0590f856916b42a9a6e300aa7be
Vincent Carluer <vcarluer@gmail.com>
2022-01-26 18:58:09 +0100

    missing vars

--- azure/new-workspace/variables.tf
+++ azure/new-workspace/variables.tf
@@ -6,6 +6,10 @@ variable "workspace_key" {
   description = "The Workspace key"
 }
 
+variable "resource_group" {
+  description = "The Azure Resource Group"
+}
+
 variable "location" {
   description = "The Azure location"
   default = "West Europe"
