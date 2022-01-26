a1f6b93ddbf5af412301cb6e1637fb47b2a12b49
Vincent Carluer <vcarluer@gmail.com>
2022-01-26 18:56:28 +0100

    missing vars

--- azure/new-workspace/variables.tf
+++ azure/new-workspace/variables.tf
@@ -1,4 +1,12 @@
-variable "rg_name" {
-  description = "resource group name"
-  default     = "terraformexample"
+variable "organization_id" {
+  description = "The Organization id"
+}
+
+variable "workspace_key" {
+  description = "The Workspace key"
+}
+
+variable "location" {
+  description = "The Azure location"
+  default = "West Europe"
 }
