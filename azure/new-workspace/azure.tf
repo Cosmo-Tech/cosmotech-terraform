1c2647a5b453667aec8b8d99dc4dd9c3d34cd7a0
Vincent Carluer <vcarluer@gmail.com>
2022-01-26 21:10:39 +0100

    credential for azuread

--- azure/new-workspace/azure.tf
+++ azure/new-workspace/azure.tf
@@ -21,5 +21,7 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  tenant_id = "${var.tenant_id}"
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
 }
