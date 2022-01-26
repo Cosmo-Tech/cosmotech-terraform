e21aaa83345601c04113cfd9bb8b986c590a853a
Vincent Carluer <vcarluer@gmail.com>
2022-01-26 18:16:18 +0100

    test change

--- azure/new-workspace/main.tf
+++ azure/new-workspace/main.tf
@@ -33,5 +33,5 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/17"]
 }
