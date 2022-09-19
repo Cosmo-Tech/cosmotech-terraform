f11c20af726df87b3158304ec5ce87c6995ee77c
jpla-csm <73175428+jpla-csm@users.noreply.github.com>
2022-09-19 16:15:18 +0200

    skip subscription provider

--- azure/new-workspace/azure.tf
+++ azure/new-workspace/azure.tf
@@ -18,6 +18,7 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+  skip_provider_registration = true
 }
 
 provider "azuread" {
