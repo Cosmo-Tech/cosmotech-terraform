1f6278076058fa2070db1c2523851bd6a967eb6a
Vincent Carluer <vcarluer@gmail.com>
2022-11-30 19:24:54 +0100

    re-output module output

--- azure/new-tenant/main.tf
+++ azure/new-tenant/main.tf
@@ -8,3 +8,24 @@ module "cosmosdb" {
   private_endpoint_vnet_name = var.private_endpoint_vnet_name
   private_endpoint_subnet_name = var.private_endpoint_vnet_name
 }
+
+output "cosmosdb_account_resource_group_name" {
+  value = module.cosmosdb.cosmosdb_account_resource_group_name
+}
+
+output "cosmosdb_account_name" {
+  value = module.cosmosdb.cosmosdb_account_name
+}
+
+output "cosmosdb_sql_database_name" {
+  value = module.cosmosdb.cosmosdb_sql_database_name
+}
+
+output "cosmosdb_account_key" {
+  sensitive = true
+  value = module.cosmosdb.cosmosdb_account_key
+}
+
+output "cosmosdb_account_uri" {
+  value = module.cosmosdb.cosmosdb_account_uri
+}
