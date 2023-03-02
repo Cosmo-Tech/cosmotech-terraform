794d87e84997d69d508372f5c3edb310538b61c0
enocquet <emmanuelnocquet8@gmail.com>
2023-03-02 15:31:09 +0100

    add default to vars and create user_app_role object

--- azure/create-platform-prerequisites/variables.tf
+++ azure/create-platform-prerequisites/variables.tf
@@ -1,29 +1,42 @@
 variable "tenant_id" {
   description = "The tenant id"
+  type = string
+  default = ""
 }
 
 variable "subscription_id" {
   description = "The subscription id"
+  type = string
+  default = ""
 }
 
 variable "client_id" {
   description = "The client id"
+  type = string
+  default = ""
 }
 
 variable "client_secret" {
   description = "The client secret"
+  type = string
+  default = ""
 }
 
 variable "platform_url" {
   description = "The platform url"
+  type = string
+  default = ""
 }
 
 variable "identifier_uri" {
   description = "The platform identifier uri"
+  type = string
+  default = ""
 }
 
 variable "stage" {
   description = "The platform stage"
+  type = string
   validation {
       condition = contains([
         "OnBoarding",
@@ -36,23 +49,30 @@ variable "stage" {
       ], var.stage)
     error_message = "Stage must be either: OnBoarding, Dev, QA, IA, EA, Demo, Prod."
   }
+  default = "Dev"
 }
 
 variable "customer" {
   description = "The customer name"
+  type = string
+  default = ""
 }
 
 variable "project" {
   description = "The project name"
+  type = string
+  default = ""
 }
 
 variable "owner_list" {
   description = "List of mail addresses for App Registration owners"
   type = list(string)
+  default = [ "" ]
 }
 
 variable "audience" {
   description = "The App Registration audience type"
+  type = string
   validation {
       condition = contains([
         "AzureADMyOrg",
@@ -65,6 +85,8 @@ variable "audience" {
 
 variable "webapp_url" {
   description = "The Web Application URL"
+  type = string
+  default = ""
 }
 
 variable "create_restish" {
@@ -135,4 +157,14 @@ variable "api_version_path" {
   description = "The API version path"
   type = string
   default = "/"
+}
+
+variable "user_app_role" {
+  type = list(object({
+    description = string
+    display_name = string
+    id = string
+    role_value = string
+  }))
+  description = "App role for azuread_application"
 }
