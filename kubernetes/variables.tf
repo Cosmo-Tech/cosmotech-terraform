3c3a6b23b2155a6796685ab4be46c0be03132a49
enocquet <emmanuelnocquet8@gmail.com>
2023-03-10 19:09:10 +0100

    forgotten vars

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -5,3 +5,19 @@ variable "resource_group" {
 variable "cluster_name" {
   type = string
 }
+
+variable "tenant_id" {
+  description = "The tenant id"
+}
+
+variable "subscription_id" {
+  description = "The subscription id"
+}
+
+variable "client_id" {
+  description = "The client id"
+}
+
+variable "client_secret" {
+  description = "The client secret"
+}
