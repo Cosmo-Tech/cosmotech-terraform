3a66fbf241f7219998c482b4b46fea1a6761fad4
enocquet <emmanuelnocquet8@gmail.com>
2023-03-10 18:36:28 +0100

    modularize kube conf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,14 +1,14 @@
-# data "azurerm_kubernetes_cluster" "example" {
-#   name                = var.cluster_name
-#   resource_group_name = var.resource_group
-# }
+data "azurerm_kubernetes_cluster" "example" {
+  name                = var.cluster_name
+  resource_group_name = var.resource_group
+}
 
-# provider "kubernetes" {
-#   host                   = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
-#   client_certificate     = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
-#   client_key             = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
-#   cluster_ca_certificate = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
-# }
+provider "kubernetes" {
+  host                   = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
+  client_certificate     = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
+}
 
 resource "kubernetes_namespace" "test" {
   metadata {
---
+++ kubernetes/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.33.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.18.1"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+provider "azuread" {
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
