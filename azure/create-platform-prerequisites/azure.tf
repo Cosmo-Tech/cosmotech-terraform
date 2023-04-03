748bb80e482c66bf2527b4f363b8f2cb8c8c8f8c
johnfolly <john.folly@cosmotech.com>
2023-04-03 16:00:51 +0200

    no az cli

--- azure/create-platform-prerequisites/azure.tf
+++ azure/create-platform-prerequisites/azure.tf
@@ -20,6 +20,7 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+  use_cli = "false"
   skip_provider_registration = true
 }
 
@@ -27,4 +28,6 @@ provider "azuread" {
   tenant_id     = var.tenant_id
   client_id     = var.client_id
   client_secret = var.client_secret
+  use_cli = "false"
+  skip_provider_registration = true
 }
